8bf4e0eb57f1838fc769b5c178fce323316a2a5e
Jason Kinney <jkinney23@yahoo.ca>
2017-09-18 14:51:47 +0400

    correct youtubescrape: title, link, thumbnail

--- youtubescrape.py
+++ youtubescrape.py
@@ -2,16 +2,23 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 if __name__ == "__main__":
-    #watch_id = input('video watch id: ')
-    youtube_url = 'https://www.youtube.com/watch?v=0uUoqD8a0V4'
+    youtube_url = input('video url: ')
+    #youtube_url = 'https://www.youtube.com/watch?v=0uUoqD8a0V4'
     page = urlopen(youtube_url).read()
     
     soup = BeautifulSoup(page, "lxml")
     recommended_videos = []
     
     for rec_video in soup.find_all("div", {"class":"content-wrapper"}):
-        recommended_videos.append(BeautifulSoup(str(rec_video)))
+        rec_video = BeautifulSoup(str(rec_video), "lxml")
+        link = rec_video.find('a')
+        href = link.get('href')
+        thumbnail = href[9:] 
+        title = link.get('title')
+        recommended_videos.append([{'title': title, 
+                                    'href': 'https://www.youtube.com' + href,
+                                    'thumbnail': 'https://i.ytimg.com/vi/' + thumbnail + '/hqdefault.jpg'}])
         
-    print(recommended_videos)
+    for video in recommended_videos:
+        print(video, '\n')
     print(len(recommended_videos))
-    
