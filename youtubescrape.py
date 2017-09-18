1cd5f7702b2c82cc6ee149103a250fdafc516438
Jason Kinney <jkinney23@yahoo.ca>
2017-09-18 16:12:53 +0400

    add write_to_csv function

--- youtubescrape.py
+++ youtubescrape.py
@@ -1,6 +1,23 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+def write_to_csv(videos):
+    
+    print('writing...')
+    outputfile = "videos.csv"
+    csv = open(outputfile, "w")
+        
+    for video in videos:
+        row = video['title'] + ', ' \
+            + video['href'] + ', ' \
+            + video['thumbnail'] + '\n'
+        print(row)
+        csv.write(row)
+        
+    csv.close()
+    
+    print('file closed')
+
 if __name__ == "__main__":
     youtube_url = input('video url: ')
     #youtube_url = 'https://www.youtube.com/watch?v=0uUoqD8a0V4'
@@ -13,12 +30,14 @@ if __name__ == "__main__":
         rec_video = BeautifulSoup(str(rec_video), "lxml")
         link = rec_video.find('a')
         href = link.get('href')
-        thumbnail = href[9:] 
         title = link.get('title')
-        recommended_videos.append([{'title': title, 
+        thumbnail = href[9:] 
+        recommended_videos.append({'title': title, 
                                     'href': 'https://www.youtube.com' + href,
-                                    'thumbnail': 'https://i.ytimg.com/vi/' + thumbnail + '/hqdefault.jpg'}])
-        
+                                    'thumbnail': 'https://i.ytimg.com/vi/' + thumbnail + '/hqdefault.jpg'})
+    '''
     for video in recommended_videos:
         print(video, '\n')
     print(len(recommended_videos))
+    '''
+    write_to_csv(recommended_videos)
